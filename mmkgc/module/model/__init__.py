a1911db4f7f152a607b6cf0bd1257a239bc812d6
liang haoxuan <12934224+l1angha0xuan@user.noreply.gitee.com>
2025-10-14 21:50:24 +0800

    1st

---
+++ mmkgc/module/model/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .Model import Model
+from .AdvRelRotatE import AdvRelRotatE
+
+__all__ = [
+    'Model',
+    'AdvRelRotatE'
+]
