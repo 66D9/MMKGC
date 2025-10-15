6cc0c4e522d26bc05521e6f0b1cc94fdd20e13ac
66D9 <572194612@qq.com>
2025-10-15 14:58:33 +0800

    only MKGY

--- mmkgc/config/__init__.py
+++ mmkgc/config/__init__.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 from .Tester import Tester
+from .Trainer import Trainer
 from .WCGTrainerGP import WCGTrainerGP
 
-__all__ = ['Tester', 'WCGTrainerGP']
+__all__ = ['Trainer', 'Tester', 'WCGTrainerGP']
